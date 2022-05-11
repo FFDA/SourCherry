c1213c566d50d4fd95f54841d98364a8fb6cf664
Domas <nebeklausk@gmail.com>
2022-05-11 17:49:41 +0300

    Implemented checkbox to open database automatically (if it isn’t protected by password);

--- app/src/main/java/com/ffda/sourcherry/MainActivity.java
+++ app/src/main/java/com/ffda/sourcherry/MainActivity.java
@@ -14,15 +14,21 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.sharedPref = getSharedPreferences(getString(R.string.com_ffda_SourCherry_PREFERENCE_FILE_KEY), Context.MODE_PRIVATE);
 
         setMessageWithDatabaseName();
 
@@ -30,19 +36,16 @@ public class MainActivity extends AppCompatActivity {
         buttonOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sharedPref = getSharedPreferences(getString(R.string.com_ffda_SourCherry_PREFERENCE_FILE_KEY), Context.MODE_PRIVATE);
-                String databaseFileExtension = sharedPref.getString("databaseFileExtension", null);
-                String databaseUri = sharedPref.getString("databaseUri", null);
-                if (databaseFileExtension.equals("ctd")) {
-                    // Works with not protected XML based databases
-                    Intent openXML = new Intent(v.getContext(), MainView.class);
-                    startActivity(openXML);
-                }
-//                } else if (databaseFileExtension.equals("ctb") || databaseFileExtension.equals("ctx")) {
-//                    // Works with SQLite based databases
-//                }
+                MainActivity.this.openDatabase();
             }
         });
+
+        CheckBox checkboxAutoOpen = (CheckBox) findViewById(R.id.checkBox_auto_open);
+        if (checkboxAutoOpen.isChecked()) {
+            if (this.sharedPref.getString("databaseUri", null) != null) {
+                this.openDatabase();
+            }
+        }
 
         //// Disables button Create Database
         // Function isn't implemented yet
@@ -61,8 +64,7 @@ public class MainActivity extends AppCompatActivity {
         String[] splitExtension = decodedUri.split("\\."); // Splitting the path to extract the file extension.
 
         // Saving filename and path to the file in the preferences
-        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.com_ffda_SourCherry_PREFERENCE_FILE_KEY), Context.MODE_PRIVATE);
-        SharedPreferences.Editor sharedPrefEditor = sharedPref.edit();
+        SharedPreferences.Editor sharedPrefEditor = this.sharedPref.edit();
 
         sharedPrefEditor.putString("databaseFilename", splitFilename[splitFilename.length - 1]);
         sharedPrefEditor.putString("databaseFileExtension", splitExtension[splitExtension.length - 1]);
@@ -87,15 +89,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setMessageWithDatabaseName() {
-        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.com_ffda_SourCherry_PREFERENCE_FILE_KEY), Context.MODE_PRIVATE);
-        EditText editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        EditText editTextTextPassword = findViewById(R.id.passwordField);
         TextView textViewMessage = findViewById(R.id.textViewMessage);
         TextView textViewPassword = findViewById(R.id.textViewPassword);
         Button buttonOpen = findViewById(R.id.button_open);
+        CheckBox checkboxAutoOpen = (CheckBox) findViewById(R.id.checkBox_auto_open);
+
+        checkboxAutoOpen.setChecked(this.sharedPref.getBoolean("checkboxAutoOpen", false));
 
         // Settings message for the user if there isn't a database selected to open
         // Otherwise displaying the name of the file
-        String databaseFilename = sharedPref.getString("databaseFilename", null);
+        String databaseFilename = this.sharedPref.getString("databaseFilename", null);
 
         if (databaseFilename == null) {
             // No file is selected
@@ -111,17 +115,43 @@ public class MainActivity extends AppCompatActivity {
             // Enabled Open button
             textViewMessage.setText(databaseFilename);
             buttonOpen.setEnabled(true);
-            String databaseFileExtension = sharedPref.getString("databaseFileExtension", null);
+            String databaseFileExtension = this.sharedPref.getString("databaseFileExtension", null);
             if (databaseFileExtension.equals("ctz") || databaseFileExtension.equals("ctx")) {
                 editTextTextPassword.setVisibility(View.VISIBLE);
                 textViewPassword.setVisibility(View.VISIBLE);
-                String tt = sharedPref.getString("databaseFileExtension", null);
             } else {
-                String tt = sharedPref.getString("databaseFileExtension", null);
                 editTextTextPassword.setVisibility(View.GONE);
                 textViewPassword.setVisibility(View.GONE);
             }
         }
+    }
+
+    private void openDatabase() {
+        if (this.sharedPref.getString("databaseFileExtension", null).equals("ctx") || this.sharedPref.getString("databaseFileExtension", null).equals("ctz")) {
+            // Checks if there is a password in the password field before opening database
+            EditText passwordField = (EditText) findViewById(R.id.passwordField);
+            if (passwordField.getText().length() <= 0) {
+                Toast.makeText(this,"Please enter password", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this,"Not implemented yet!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            try {
+                Intent openDatabase = new Intent(this, MainView.class);
+                startActivity(openDatabase);
+            } catch (Exception e) {
+                Toast.makeText(this,"Failed to open database!", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    public void saveCheckboxStatus(View view) {
+        CheckBox checkBoxAutoOpen = (CheckBox) findViewById(R.id.checkBox_auto_open);
+
+        SharedPreferences.Editor sharedPrefEditor = this.sharedPref.edit();
+        sharedPrefEditor.putBoolean("checkboxAutoOpen", checkBoxAutoOpen.isChecked());
+        sharedPrefEditor.commit();
     }
 
 }
